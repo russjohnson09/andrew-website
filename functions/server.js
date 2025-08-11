768739a910788d0f3f2e9d707e841ee0531cd628
Russ Johnson <rjohnson@retailcapital.com>
2025-08-11 08:32:21 -0400

    react build

--- functions/server.js
+++ functions/server.js
@@ -1,25 +1,10 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-
-
-
-router.get('/test', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
+const {router} = require('../src/router')
 
 
 app.use(bodyParser.json());
@@ -33,7 +18,7 @@ app.use('/.netlify/functions/server', router);  // path must route to lambda
 // npm run build
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../react-app/build/.html')));
 
-app.use(express.static(path.join(__dirname, '../react-app/build')));
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
--- server-local.js
+++ server-local.js
@@ -1,5 +1,13 @@
-'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const app = require('./functions/server');
+
+const { router } = require('./src/router')
+
+
+app.use(bodyParser.json());
+
+app.use('/', router);
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
