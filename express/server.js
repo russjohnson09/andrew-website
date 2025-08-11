12028cf0694db8d8c66de0ba10e0b1aa74a5fb15
Russ Johnson <rjohnson@retailcapital.com>
2025-08-11 08:50:48 -0400

    update

--- express/server.js
+++ express/server.js
@@ -8,10 +8,10 @@ const {router} = require('../src/router')
 
 
 // https://docs.netlify.com/api-and-cli-guides/cli-guides/manage-functions/
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 
-app.use('/.netlify/functions', router);  // path must route to lambda
+// app.use('/.netlify/functions', router);  // path must route to lambda
 // app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 // app.use('/', router);  // path must route to lambda
@@ -22,7 +22,15 @@ app.use('/.netlify/functions', router);  // path must route to lambda
 // npm run build
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../react-app/build/.html')));
 
+// https://github.com/neverendingqs/netlify-express/blob/master/express/server.js
+
+// https://answers.netlify.com/t/deploying-express-server-on-netlify-with-lambda-functions/19535
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+
