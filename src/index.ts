243524035783e9952d4e7bb85153822897d13bd8
Russ Johnson <rjohnson@retailcapital.com>
2025-08-11 12:42:21 -0400

    update

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// netlify's entrypoint is netlify/functions/api.ts
+// This is for local testing.
+// Both have a shared entry point of router
+import { router } from './router'
+
+const app = express();
+const port = 3000;
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
