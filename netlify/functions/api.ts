1d89980603e902c34fb1b7f5dbc143a6eac2281c
Russ Johnson <rjohnson@retailcapital.com>
2025-08-11 11:49:59 -0400

    router update

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -13,13 +13,14 @@
 
 
 // https://docs.netlify.com/build/frameworks/framework-setup-guides/express/
+// https://docs.netlify.com/build/functions/get-started/?data-tab=TypeScript
 import express, { Router } from "express";
 import serverless from "serverless-http";
 // const { router } = require('../../src/router');
-
+import { router } from '../../src/router';
 const api = express();
 
-const router = Router();
+// const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
