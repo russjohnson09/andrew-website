bd5c4913d0cff587707a13f25c1ae29ef18eb38d
Russ Johnson <rjohnson@retailcapital.com>
2025-08-11 09:56:51 -0400

    import

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,26 @@
-const express = require("express");
-const serverless = require("serverless-http");
+// const express = require("express");
+// const serverless = require("serverless-http");
 
-const { router } = require('../../src/router');
+// const { router } = require('../../src/router');
+
+// const api = express();
+
+// // const router = Router();
+// // router.get("/hello", (req, res) => res.send("Hello World!"));
+
+// api.use("/", router);
+
+// export const handler = serverless(api);
+
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
 const api = express();
 
-// const router = Router();
-// router.get("/hello", (req, res) => res.send("Hello World!"));
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/", router);
+api.use("/api/", router);
 
 export const handler = serverless(api);
